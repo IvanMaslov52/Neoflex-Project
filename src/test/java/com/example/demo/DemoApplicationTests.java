261e7b88b0f22c1ffca41e2dcf0b3b0e619827e6
Papagoth <ivanmaslov19911@gmail.com>
2022-10-18 18:42:59 +0400

    added test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.Services.CalculatorService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private CalculatorService calculatorService;
 	@Test
-	void contextLoads() {
+	void testingCalculate(){
+		Assertions.assertEquals(new BigDecimal("10000.00"), calculatorService.calculate(BigDecimal.valueOf(29300),10));
+	}
+
+	@Test
+	void testingCalculateWithDates(){
+		Assertions.assertEquals(new BigDecimal("5000.00"), calculatorService.calculate(BigDecimal.valueOf(29300), LocalDate.of(2022, 10, 10), LocalDate.of(2022, 10, 16)));
 	}
 
 }
